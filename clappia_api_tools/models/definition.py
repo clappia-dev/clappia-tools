35e206aca01b5368c76ed694d243dbb5ccf416fe
Rishabh Verma <rishabh.v@clappia.com>
2025-11-12 16:31:30 +0530

    Update section_type field in ExternalSectionDetails and UpsertSectionRequest models to use lowercase literals for consistency

--- clappia_api_tools/models/definition.py
+++ clappia_api_tools/models/definition.py
@@ -86,8 +86,8 @@ class ExternalSectionDetails(BaseFieldComponent):
     )
     retain_values: bool = Field(False, description="Retain values when hidden")
     keep_section_collapsed: bool = Field(False, description="Keep section collapsed")
-    section_type: Literal["Section", "Table"] = Field(
-        "Section", description="Type of the section"
+    section_type: Literal["section", "table"] = Field(
+        "section", description="Type of the section"
     )
     initial_rows: int = Field(5, description="Initial number of rows")
 
--- clappia_api_tools/models/request/app_definition/section.py
+++ clappia_api_tools/models/request/app_definition/section.py
@@ -40,7 +40,7 @@ class UpsertSectionRequest(BaseFieldComponent):
     )
     retain_values: bool = Field(False, description="Retain values when hidden")
     keep_section_collapsed: bool = Field(False, description="Keep section collapsed")
-    section_type: Literal["Section", "Table"] = Field(
-        "Section", description="Type of the section"
+    section_type: Literal["section", "table"] = Field(
+        "section", description="Type of the section"
     )
     initial_rows: int = Field(5, description="Initial number of rows")
